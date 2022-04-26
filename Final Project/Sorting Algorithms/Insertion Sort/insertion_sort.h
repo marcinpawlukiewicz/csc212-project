32590987eee81a3dc296db669994e270a20434ad
lisandro2002 <71111178+lisandro2002@users.noreply.github.com>
2022-04-26 14:21:59 -0400

    Create insertion_sort.h

---
+++ Final Project/Sorting Algorithms/Insertion Sort/insertion_sort.h
@@ -0,0 +1,18 @@
+#ifndef __INSERTION_SORT_H__
+#define __INSERTION_SORT_H__
+
+#include <vector>
+#include <iostream>
+#pragma once
+
+class InsertionSort{
+
+    private:
+    
+    public:
+        // declaring the insertion sort constructor that would take a vector
+        InsertionSort(std::vector<int> arr);
+
+};
+
+#endif
